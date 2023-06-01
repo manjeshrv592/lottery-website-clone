909b0c8b0d9744e198be4b721c72acbac09a749a
Manjesh R V <manjeshrv592@gmail.com>
2023-06-01 12:59:00 +0530

    Added hero carousel

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.hero-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
