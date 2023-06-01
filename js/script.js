9bca5c960e401c97812162877ed3f911bceb9cac
Manjesh R V <manjeshrv592@gmail.com>
2023-06-01 17:13:06 +0530

    Added lottery section

--- js/script.js
+++ js/script.js
@@ -3,12 +3,44 @@ const swiper = new Swiper('.hero-slider', {
   direction: 'horizontal',
   loop: true,
 
-  // autoplay: {
-  //   delay: 3000,
-  // },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 });
+
+const lotteryCardSwiperProps = {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+};
+
+const lotterySwiperOne = new Swiper(
+  '.lottery-card-slider-1',
+  lotteryCardSwiperProps
+);
+
+const lotterySwiperTwo = new Swiper(
+  '.lottery-card-slider-2',
+  lotteryCardSwiperProps
+);
+
+const lotterySwiperThree = new Swiper(
+  '.lottery-card-slider-3',
+  lotteryCardSwiperProps
+);
+
+const lotterySwiperFour = new Swiper(
+  '.lottery-card-slider-4',
+  lotteryCardSwiperProps
+);
